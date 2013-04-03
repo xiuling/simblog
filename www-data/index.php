82209c8ccafa39bfe10ed6dfb247b5645c78e390
xling <xling@mint>
2013-04-03 23:54:56 +0800

    change index style

--- www-data/index.php
+++ www-data/index.php
@@ -1,9 +1,13 @@
 <?php
 	include 'db.inc.php';
+
+
     $db = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD) or die ('Unable to connect. Check your connection parameters.');
     mysql_select_db(MYSQL_DB, $db) or die(mysql_error($db));
 
     include 'header.inc.php';
+
+
     $query = 'SELECT cid,title,slug,type,`text`,created FROM contents WHERE type!="personal" and type!="about" ORDER BY created ';
     $result = mysql_query($query, $db) or die (mysql_error($db));
     if (mysql_num_rows($result) > 0) {
@@ -16,6 +20,8 @@
 		}
 	}
     echo ' </div> ';
+
+    
     include 'sidebar.php';
 	include 'foot.inc.php';
 ?>
